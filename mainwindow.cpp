f7d4de1680d6e45d0734f1aaddb47ac7b6158179
kb4 <kent_brockman4@mail.ru>
2018-12-04 22:23:27 +0700

    first graph

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    configurePlot();
+    configureGraph();
+
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(drawData()));
+    timer->start(100);
+}
+
 void MainWindow::configurePlotEnergyCurrent(QCustomPlot *plot, const QString &y1Label, const QString &y2Label)
 {
     plot->xAxis->setLabel("Время");
@@ -15,22 +30,49 @@ void MainWindow::configurePlotEnergyCurrent(QCustomPlot *plot, const QString &y1
 
     connect(plot->xAxis,SIGNAL(rangeChanged(QCPRange)),this,SLOT(changeRanges(QCPRange)));
 
+
     plot->xAxis->setDateTimeFormat("hh:mm:ss");
     plot->xAxis->setTickLabelType(QCPAxis::ltDateTime);
 }
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+void MainWindow::drawData()
 {
-    ui->setupUi(this);
+    auto date = QDateTime::currentDateTime().toTime_t();
+    graphEnergy->addData(date, sin(date));
+
+    if (ui->checkBox->isChecked())
+        ui->plotEnergyCurrent->xAxis->setRange(date - plotScreenBufferSEC, date);
+    ui->plotEnergyCurrent->replot();
+}
+
+void MainWindow::configureGraphEnergyCurrent()
+{
+    graphEnergy = ui->plotEnergyCurrent->addGraph();
+    graphEnergy->setPen(QColor(0xAA, 0xBB, 0xCC));
+
+    graphCurrent = ui->plotEnergyCurrent->addGraph();
+    graphCurrent->setPen(QColor(0x0A, 0x0B, 0x0C));
+}
+
+void MainWindow::configureGraph()
+{
+    configureGraphEnergyCurrent();
+
+    graphTemperature = ui->plotTemperaturePower->addGraph();
+    graphTemperature->setPen(QColor(0xAA, 0xBB, 0xCC));
+}
+
+void MainWindow::configurePlot()
+{
     configurePlotEnergyCurrent(ui->plotEnergyCurrent, "Энергия (кВ)", "Ток (мА)");
     configurePlotEnergyCurrent(ui->plotTemperaturePower, "Температура (С)", "Мощность (Вт)");
     configurePlotEnergyCurrent(ui->plotVacuumRadiation, "Вакуум (Pa)", "Радиация (Зв)");
 
     connect(ui->plotEnergyCurrent,SIGNAL(afterReplot()),ui->plotTemperaturePower,SLOT(replot()));
     connect(ui->plotEnergyCurrent,SIGNAL(afterReplot()),ui->plotVacuumRadiation,SLOT(replot()));
+
 }
+
 
 MainWindow::~MainWindow()
 {
@@ -51,3 +93,8 @@ void MainWindow::changeRanges(QCPRange range)
 
 
 
+
+void MainWindow::on_checkBox_stateChanged(int arg1)
+{
+    plotUpdateRealTIme = static_cast<bool>(arg1);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -3,11 +3,14 @@
 
 #include <QMainWindow>
 #include <QBoxLayout>
+#include <QTimer>
 #include "qcustomplot.h"
 
 namespace Ui {
 class MainWindow;
 }
+
+class QCPAxis;
 
 class MainWindow : public QMainWindow
 {
@@ -18,12 +21,28 @@ public:
     ~MainWindow();
 
 
+
+    void configureGraphEnergyCurrent();
+
 private:
     Ui::MainWindow *ui;
     void configurePlotEnergyCurrent(QCustomPlot *plot, const QString &y1Label, const QString &y2Label);
+    bool plotUpdateRealTIme;
+    int plotScreenBufferSEC = 120;
+
+    QTimer *timer;
+
+    QCPGraph *graphEnergy, *graphCurrent, *graphTemperature;
+
+
+    void configureGraph();
+    void configurePlot();
+
 
 private slots:
+    void drawData();
     void changeRanges(QCPRange range);
+    void on_checkBox_stateChanged(int arg1);
 };
 
 #endif // MAINWINDOW_H
