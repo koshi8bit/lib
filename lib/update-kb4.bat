439b27594e44162dea2ad0530d7bc506b18f81b6
kb4 <kent_brockman4@mail.ru>
2019-02-12 17:53:20 +0700

    update-kb4.bat

--- lib/update-kb4.bat
+++ lib/update-kb4.bat
@@ -1,7 +1,7 @@
+::COPY TO lib\
 @echo off
 set source="G:\kent_brockman4\prog\C\Qt\lib\kb4\prj\lib\kb4"
 set dest=%cd%\kb4\
-echo %dest%
 rd %dest% /Q /S
 xcopy %source% %dest% /E /Y
 attrib %dest%* /S +R
